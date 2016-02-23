ae1739faead7dd79b1b23c0b077a13e0e89b818d
Eric Walsh <walshericc@gmail.com>
2016-02-23 15:56:30 -0500

    Updated Scene 0 with suggested changes and added current assets for Scene 1

---
+++ Assets/Scripts/OnHover.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OnHover : MonoBehaviour {
+
+    bool isHoverActive = false;
+
+    public void SetHoverActive()
+    {
+        isHoverActive = true;
+    }
+
+    void OnMouseOver()
+    {
+        if (isHoverActive)
+            AdjustAlpha(0.5f);
+    }
+
+    void OnMouseExit()
+    {
+        if (isHoverActive)
+            AdjustAlpha(1f);
+    }
+
+    void AdjustAlpha (float amt)
+    {
+        Color clr = GetComponent<SpriteRenderer>().color;
+        clr.a = amt;
+        GetComponent<SpriteRenderer>().color = clr;
+    }
+}
---
+++ Assets/Scripts/OnHoverEnlarge.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OnHoverEnlarge : MonoBehaviour {
+
+    bool isHoverActive = false;
+
+    public void SetHoverActive()
+    {
+        isHoverActive = true;
+    }
+
+    void OnMouseOver()
+    {
+        if (isHoverActive)
+            AdjustScale(2f);
+    }
+
+    void OnMouseExit()
+    {
+        if (isHoverActive)
+            AdjustScale(1f);
+    }
+
+    void AdjustScale(float amt)
+    {
+        transform.localScale = new Vector3(amt, amt, 0f);
+    }
+}
